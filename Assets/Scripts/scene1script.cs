f834ab337673c7a17d9ac62f33e6a852646bd024
Sarina Li <74844953+sarinali@users.noreply.github.com>
2024-01-28 03:11:38 -0500

    menu

--- Assets/Scripts/scene1script.cs
+++ Assets/Scripts/scene1script.cs
@@ -12,12 +12,17 @@ public class scene1script : MonoBehaviour
     private readonly int duration = 15;
     private float time = 0;
     public CanvasGroup fadePanel;
-    public float fadeDuration = 2f; 
+    public float fadeDuration = 2f;
+    public GameObject obj1;
+    public GameObject obj2;
+    public GameObject obj3;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        obj1.SetActive(false);
+        obj2.SetActive(false);
+        obj3.SetActive(false);
     }
 
     IEnumerator FadeOut(string sceneToLoad)
@@ -38,7 +43,11 @@ public class scene1script : MonoBehaviour
         time += Time.deltaTime;
         if (time >= 15)
         {
-            SceneManager.LoadScene("MenuScene");
+            message.SetText("");
+            obj1.SetActive(true);
+            obj2.SetActive(true);
+            obj3.SetActive(true);
+            // SceneManager.LoadScene("MenuScene");
         }
         else if (time >= 10)
         {
